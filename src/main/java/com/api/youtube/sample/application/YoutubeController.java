87750ed5a06cd9beb155ec5f8e58546659aa9696
dev-fjk <fjk.develop@gmail.com>
2021-10-16 14:53:17 +0900

    みこちの動画一覧をJson形式で取得できるように修正

--- src/main/java/com/api/youtube/sample/application/YoutubeController.java
+++ src/main/java/com/api/youtube/sample/application/YoutubeController.java
@@ -1,6 +1,10 @@
 package com.api.youtube.sample.application;
 
 import com.api.youtube.sample.config.YoutubeConfig;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -13,22 +17,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "api/v1/")
+@RequestMapping(value = "youtube/api/v1/")
 @RequiredArgsConstructor
 public class YoutubeController {
 
+    private final YouTube youtubeDataApi;
     private final YoutubeConfig youtubeConfig;
 
-    @GetMapping(value = "get")
-    public ResponseEntity<Integer> getMovies() {
+    @GetMapping(value = "get/movies")
+    public ResponseEntity<?> getMovies() {
         log.info("Youtube Controller Get Http Request");
-        
-        String id = youtubeConfig.getChannelId();
-        String key = youtubeConfig.getKey();
-        log.info("id : {} , key : {}", id, key);
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        return new ResponseEntity<>(1, httpHeaders, HttpStatus.OK);
+
+        try {
+            // TODO 出来れば レスポンスで返したJsonをViewに渡して画面表示させたい
+            // 検索の実行 動画一覧をJsonで取得できる
+            SearchListResponse response = this.createSearchApi().execute();
+            return new ResponseEntity<>(response.toPrettyString(), httpHeaders, HttpStatus.OK);
+        } catch (Exception exception) {
+            // エラーレスポンスの作成
+            log.error("Youtubeへのリクエスト送信に失敗しました。", exception);
+            Map<String, Object> errorDetails = new HashMap<>();
+            errorDetails.put("error", "Youtube Connect Error");
+            errorDetails.put("status", HttpStatus.BAD_REQUEST.value());
+            return new ResponseEntity<>(errorDetails, httpHeaders, HttpStatus.OK);
+        }
+    }
+
+    /**
+     * Youtubeの検索条件の設定
+     *
+     * @return : 検索用クラス
+     * @throws Exception : 設定時例外
+     */
+    private YouTube.Search.List createSearchApi() throws Exception {
+
+        YouTube.Search.List search = youtubeDataApi.search().list("id,snippet");
+        search.setChannelId(youtubeConfig.getChannelId());
+        search.setKey(youtubeConfig.getKey());
+        search.setQ("Minecraft"); // みこちのマイクラ動画でフィルタする
+        search.setType("video");  // 検索対象のTypeを設定。他にchannelとplaylistが設定できる
+        search.setMaxResults(10L); // 取得するヒット数の最大値を設定
+        search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+        return search;
     }
 }
--- src/main/java/com/api/youtube/sample/config/YoutubeConfig.java
+++ src/main/java/com/api/youtube/sample/config/YoutubeConfig.java
@@ -1,8 +1,14 @@
 package com.api.youtube.sample.config;
 
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
@@ -11,8 +17,29 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "extension.youtube")
 public class YoutubeConfig {
 
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
     // youtubeのチャンネルID
     private String channelId;
     // Youtube Data Api の APIキー
     private String key;
+
+    /**
+     * Youtubeとの接続セットアップ
+     *
+     * @return : Youtube
+     */
+    @Bean(name = "youtubeDataApi")
+    public YouTube youtubeDataAPi() {
+        return new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+                request -> {
+                }).setApplicationName("youtube-search-api").build();
+    }
 }
